a2f976b864b2069b7c82fcec987ba634162509bc
Maksim <vl4dimirov@gmail.com>
2024-01-12 15:59:39 +0300

    add: main page

---
+++ src/components/CardList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Card from './Card.vue'
+const onClickAdd = () => {
+    alert('add')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      imageUrl="/sneakers/sneakers-1.jpg"
+      title="Зеленый кросовка"
+      :price="5000"
+      :isAdded="true"
+      :isFavorite="true"
+      :onCLickAdd="onClickAdd"
+    />
+  </div>
+</template>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Красовка</h2>
+</template>
